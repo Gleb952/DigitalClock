7fb93991511fb601c17217329baf65c9d61262df
Gleb952 <lomakin.plex@mail.ru>
2022-10-11 21:34:03 +0300

    .toString

--- js/style.js
+++ js/style.js
@@ -5,9 +5,9 @@ function digital_clock () {
 
     var time = new Date();
 
-    var h = time.getHours();
-    var m = time.getMinutes();
-    var s = time.getSeconds();
+    var h = time.getHours().toString();
+    var m = time.getMinutes().toString();
+    var s = time.getSeconds().toString();
 
     if (h.length < 2){
         h = "0" + h;
